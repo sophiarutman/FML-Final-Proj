06c911f98798cde3dfea0426d76654f78c2f2ea1
John Soeder <jsoeder20@gmail.com>
2023-05-15 14:39:21 -0400

    DQN Updates
    Fixed the reward function

--- dqn_train.py
+++ dqn_train.py
@@ -5,8 +5,8 @@ import sys
 
 
 symbol = "AMZN"
-window_size = 1
-episode_count = 2
+window_size = 30
+episode_count = 4
 start, end = "2018-01-01", "2020-12-31"
 
 agent = Agent()
@@ -18,21 +18,24 @@ max_rsi =  max(data["RSI"])
 max_macd = max(data["MACD"])
 max_lobb = max(data["Lobbying"])
 
-l = len(data[symbol]) - 1
+l = len(data[symbol]) 
 batch_size = 16
+total_profit = 0
 
-for e in range(episode_count + 1):
+for e in range(episode_count):
 	print("Episode " + str(e) + "/" + str(episode_count))
 	state = getState(data, symbol, 0, max_price, max_macd, max_rsi, max_lobb)
 	state = np.append(state, 0)
 	state = np.expand_dims(state, axis=0)  # Add an extra dimension at axis=0
-
+	pa = 0
+	last_price = data[symbol].values[0]
 
 	total_profit = 0
 	agent.inventory = []
 
 	for t in range(l):
 		action = agent.act(state)
+		
 
 		next_state = getState(data, symbol, t + 1, max_price, max_macd, max_rsi, max_lobb)
 		next_state = np.append(next_state, action)
@@ -43,28 +46,68 @@ for e in range(episode_count + 1):
 		price =  data[symbol].values[t]
 		holdings = 0
 
-		if action == 1: # buy
-			holdings = 1000
-			agent.inventory.append(price)
-			reward = max( - price, 0)
-			total_profit -= (price * 1000)
-			print("Buy: " + formatPrice(price))
+		if action == 1 and pa != 1: #long
+			if pa == 0:
+				reward = 0
+				last_price = price
+				total_profit -= (price * 1000)
+				print("Buy at: " + formatPrice(price))
+			elif pa == 2:
+				reward = max(last_price - price, 0)
+				# Short to Flat
+				total_profit -= (price * 1000)
+				# Flat to Long
+				total_profit -= (last_price * 1000)
+				last_price = price
+				print("Short to Long at: " + formatPrice(price))
 
-		elif action == 2 and len(agent.inventory) > 0: # sell
-			bought_price = agent.inventory.pop(0)
-			reward = max(price - bought_price, 0)
-			total_profit += (price - bought_price) * 1000
-			print("Sell: " + formatPrice(price) + " | Profit: " + formatPrice(price - bought_price) * 1000)
+		elif action == 2 and pa != 2: # short
+			if pa == 0:
+				reward = 0
+				last_price = price
+				total_profit += (price * 1000)
+				print("Short at: " + formatPrice(price))
+			elif pa == 1:
+				reward = max(price - last_price, 0)
+				last_price = price
+				# Long to Flat
+				total_profit += (price * 1000)
+				# Flat to Short
+				total_profit += (price * 1000)
+				print("Long to Short at: " + formatPrice(price))
 		
+		elif action == 0 and pa != 0: #flat
+			if pa == 1: #Long to flat
+				reward = max(price - last_price, 0)
+				# Long to Flat
+				total_profit += (price * 1000)
+				print("Long to Flat at: " + formatPrice(price))
+
+			elif pa == 2: #Short to flat
+				reward = max(last_price - price, 0)
+				# Short to Flat
+				total_profit -= (price * 1000)
+				print("Short to Flat at: " + formatPrice(price))
+
+		print("Profit: " + formatPrice(total_profit))
+		pa = action
 
 		done = True if t == l - 1 else False
 		agent.memory.append((state, action, reward, next_state, done))
 		state = next_state
 
 		if done:
+
+			if (action == 1):
+				total_profit += (price * 1000)
+			elif (action == 2):
+				total_profit -= (price * 1000)
+
 			print("--------------------------------")
 			print("Total Profit: " + formatPrice(total_profit))
 			print("--------------------------------")
+
+
 
 		if len(agent.memory) > batch_size:
 			agent.expReplay(batch_size)
